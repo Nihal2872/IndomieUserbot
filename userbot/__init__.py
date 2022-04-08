54b3e7b23a66531a5e73389150d960b8ced21236
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-04-09 02:51:02 +0700

    ㅤ

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -110,10 +110,7 @@ while 0 < 6:
 
 del _DEVS
 
-SUDO_USERS = {
-    int(x) for x in os.environ.get(
-        "SUDO_USERS",
-        "1447438514").split()}
+SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
 BL_CHAT = {int(x) for x in os.environ.get("BL_CHAT", "").split()}
 BLACKLIST_GCAST = {int(x) for x in os.environ.get("BLACKLIST_GCAST", "").split()}
 

b80180e692f0e92a8138986c420b5902f9e310da
IndomieGorengSatu <rezagaming61@gmail.com>
2022-04-09 20:33:11 +0000

    pylint: auto fixes
    Signed-off-by: IndomieGorengSatu <rezagaming61@gmail.com>

--- userbot/utils/botlogs.py
+++ userbot/utils/botlogs.py
@@ -1,37 +1,16 @@
-import asyncio
-import importlib
-import logging
-import sys
-from pathlib import Path
-from random import randint
 import os
-import random
-import time
-from random import randint
 
 from telethon.errors import (
-    BotMethodInvalidError,
-    ChannelPrivateError,
     ChannelsTooMuchError as penuh_tolol,
-    ChatAdminRequiredError,
-    UserNotParticipantError,
 )
-from telethon.tl.custom import Button
 from telethon.tl.functions.channels import (
-    CreateChannelRequest,
-    EditAdminRequest,
     EditPhotoRequest as oh,
-    InviteToChannelRequest,
-    JoinChannelRequest,
 )
-from telethon.tl.functions.contacts import UnblockRequest
-from telethon.tl.types import ChatAdminRights, InputChatUploadedPhoto, InputMessagesFilterDocument
 from telethon.tl.types import ChatPhotoEmpty as jembut
-from telethon.utils import get_peer_id
 
 import heroku3
 
-from userbot import BOTLOG_CHATID, tgbot, bot, HEROKU_API_KEY, HEROKU_APP_NAME, LOGS, BOT_USERNAME
+from userbot import BOTLOG_CHATID, HEROKU_API_KEY, HEROKU_APP_NAME, LOGS, bot
 
 heroku_api = "https://api.heroku.com"
 if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
@@ -42,9 +21,11 @@ else:
     app = None
 
 # by : kenkan
+
+
 async def autopilot():
     if BOTLOG_CHATID and str(BOTLOG_CHATID).startswith("-100"):
-      return
+        return
     k = []  # To Refresh private ids
     async for x in bot.iter_dialogs():
         k.append(x.id)
@@ -57,8 +38,8 @@ async def autopilot():
     try:
         r = await bot(
             penuh_tolol(
-                title ="Log UserBot",
-                desc ="Group Log IndomieUserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✦ Powered By ~ @IndomieProject ✦",
+                title="Log UserBot",
+                desc="Group Log IndomieUserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✦ Powered By ~ @IndomieProject ✦",
                 megagroup=True,
             ),
         )

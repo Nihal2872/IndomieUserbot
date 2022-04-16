d93738a3c1538de7a27637cb1924b3ec1785298c
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-04-17 01:08:59 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -3,43 +3,45 @@
 import asyncio
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_delete, edit_or_reply, indomie_cmd
 
 
-@register(outgoing=True, pattern=r"^\.logo(?: |$)(.*)")
+@indomie_cmd(pattern="logo(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
     aing = await event.client.get_me()
     text = event.pattern_match.group(1)
     if not text:
-        await event.edit("`Give a name too!`")
+        await edit_delete(event, "**Silahkan Masukan Text Untuk Logo**")
     else:
-        await event.edit("`Processing`")
-    chat = "@tdtapibot"
+        await edit_or_reply(event, "`Processing...`")
+    chat = "@IndomieMusicRobot"
     async with event.client.conversation(chat) as conv:
         try:
             msg = await conv.send_message(f"/logo {text}")
             response = await conv.get_response()
             logo = await conv.get_response()
-            """ - don't spam notif - """
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit(
-                "**Error: Mohon Buka Blokir** @Nastymusiicbot **Dan Coba Lagi!**"
-            )
-            return
+            await event.client(UnblockRequest(chat))
+            msg = await conv.send_message(f"/logo {text}")
+            response = await conv.get_response()
+            logo = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
         await asyncio.sleep(0.5)
         await event.client.send_file(
             event.chat_id,
             logo,
-            caption=f"Logo by [{ALIVE_NAME}](tg://user?id={aing.id})",
+            caption=f"Logo by [{aing.first_name}](tg://user?id={aing.id})",
         )
         await event.client.delete_messages(conv.chat_id, [msg.id, response.id, logo.id])
         await event.delete()
 
 
-CMD_HELP.update({"logo": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.logo <text>`"
-                 "\n↳ : Hasilkan logo dari Teks atau Balas Ke gambar yang diberikan, untuk menulis teks Anda di atasnya. Atau Balas Ke File Font, Untuk menulis dengan font itu."})
+CMD_HELP.update({"logo": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}logo <text>`"
+                 "\n↳ : **Membuat logo dari Teks yang diberikan.**"})

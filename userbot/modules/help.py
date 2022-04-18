a2007f1c530f4619c2e62f7dfb7fb5a6b5fca1f5
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-04-19 03:08:22 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -3,40 +3,39 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot help command """
 
-import asyncio
-from userbot import CMD_HELP
+from userbot import CHANNEL, CHANNEL2
+from userbot import CMD_HELP, ICON_HELP, ch
 from userbot import CMD_HANDLER as cmd
-from userbot.utils import indomie_cmd
+from userbot.utils import edit_delete, edit_or_reply, indomie_cmd
 
 modules = CMD_HELP
 
 
 @indomie_cmd(pattern="help(?: |$)(.*)")
 async def help(indomie):
-    """ For .help command,"""
     args = indomie.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await indomie.edit(str(CMD_HELP[args]))
+            await edit_or_reply(indomie, f"{CMD_HELP[args]}\n\n© {ch}")
         else:
-            await indomie.edit("**`NGETIK YANG BENER NGENTOT!`**")
-            await asyncio.sleep(50)
-            await indomie.delete()
+            await edit_delete(indomie, f"`{args}`**NGETIK YANG BENER NGENTOT!!.**")
     else:
         memek = await indomie.client.get_me()
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t ✦  "
-        await indomie.edit(f"**• List Help [IndomieUserbot](https://github.com/IndomieGorengSatu/IndomieUserbot):**\n\n"
-                           f"**• Jumlah** `{len(CMD_HELP)}` **Modules**\n"
-                           f"**• Bᴏᴛ Oᴡɴᴇʀ :** [{memek.first_name}](tg://user?id={memek.id})\n\n"
-                           "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                           f"✦ {string}✦\n\n"
-                           "\n\nSupport @IndomieProject"
-                           "\nStore @IndomieStore",
-                           )
+            string += f"`\t\t\t{ICON_HELP}\t\t\t"
+        await edit_or_reply(
+            indomie,
+            f"**• List Help [IndomieUserbot](https://github.com/IndomieGorengSatu/IndomieUserbot):**\n\n"
+            f"**• Jumlah** `{len(CMD_HELP)}` **Modules**\n"
+            f"**• Bᴏᴛ Oᴡɴᴇʀ :** [{memek.first_name}](tg://user?id={memek.id})\n\n"
+            "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+            f"{ICON_HELP}   {string}"
+            f"\n\nSupport @{CHANNEL}"
+            f"\n\nSupport @{CHANNEL2}",
+        )
         await indomie.reply(
-            f"\n✎ **Contoh :** `<{cmd}help ping>` **Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba yahahaha...**")
+            f"\n**Contoh Ketik** `{cmd}help ping` **Untuk Melihat Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba yahahaha...**"
+        )

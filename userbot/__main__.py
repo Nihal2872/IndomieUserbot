a4551daa2bc0160322f41178bf7b84c35fd69fe5
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-04-22 22:22:49 +0700

    testing

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -22,7 +22,7 @@ from telethon import version
 
 from userbot import BOT_TOKEN
 from userbot import BOT_VER as ubotversion
-from userbot import BOTLOG_CHATID, LOGS, LOOP, bot
+from userbot import BOTLOG_CHATID, LOGS, bot
 from userbot.clients import memek_userbot_on, multimemek
 from userbot.core.git import git
 from userbot.modules import ALL_MODULES
@@ -30,37 +30,35 @@ from userbot.modules.assistant import ASST_MODULES
 from userbot.utils import autobot, autopilot
 
 try:
-    for module_name in ALL_MODULES:
-        imported_module = import_module(f"userbot.modules.{module_name}")
-    for module_name in ASST_MODULES:
-        imported_module = import_module(
-            f"userbot.modules.assistant.{module_name}")
     client = multimemek()
     total = 5 - client
     git()
     LOGS.info(f"Total Clients = {total} User")
-    LOGS.info(f"Python Version - {python_version()}")
-    LOGS.info(f"Telethon Version - {version.__version__}")
-    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-    LOGS.info(
-        f"IndomieUserbot Version - {ubotversion} [ BERHASIL DIAKTIFKAN! ]")
-except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
-    pass
 except BaseException as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
 
+for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
 
-LOOP.run_until_complete(memek_userbot_on())
+for module_name in ASST_MODULES:
+        imported_module = import_module("userbot.modules.assistant." + module_name)
+
+
+bot.loop.run_until_complete(memek_userbot_on())
 if not BOTLOG_CHATID:
-    LOOP.run_until_complete(autopilot())
+    bot.lopp.run_until_complete(autopilot())
 if not BOT_TOKEN:
-    LOOP.run_until_complete(autobot())
+    LOGS.info(
+        "BOT_TOKEN Vars kaga di isi, otewe bikin bot di @Botfather ngeeeng..."
+    )
+    bot.loop.run_until_complete(autobot())
+LOGS.info(f"Python Version - {python_version()}")
+LOGS.info(f"Telethon Version - {version.__version__}")
+LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
+LOGS.info(f"IndomieUserbot Version - {ubotversion} [ BERHASIL DIAKTIFKAN! ]")
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
-    try:
-        bot.run_until_disconnected()
-    except ConnectionError:
-        pass
+    bot.run_until_disconnected()

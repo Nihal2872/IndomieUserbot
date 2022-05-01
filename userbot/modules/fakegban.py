09844524caa7f217460575f51c4f51cf9bbeb6c4
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-05-02 01:40:21 +0700

    delete ALIVE_NAME

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -3,13 +3,8 @@
 import asyncio
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
-#from userbot.utils import admin_cmd
 from userbot.events import register
-from userbot import ALIVE_NAME, CMD_HELP, bot
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HELP, bot, owner
 
 
 @register(outgoing=True, pattern="^.fgban(?: |$)(.*)")
@@ -18,7 +13,7 @@ async def gbun(event):
         return
     gbunVar = event.text
     gbunVar = gbunVar[6:]
-    mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
+    mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {owner}\n"
     no_reason = "No Reason Given "
     await event.edit("**Summoning out the mighty gban hammer ☠️**")
     asyncio.sleep(3.5)
@@ -33,12 +28,12 @@ async def gbun(event):
         usname = replied_user.user.username
         idd = reply_message.from_id
         # make meself invulnerable cuz why not xD
-        if idd == 1036951071:
-            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Heyworld](tg://user?id=1036951071) __to release your account__😏")
+        if idd == 1447438514:
+            await reply_message.reply("`Wait a second, This is my master!`\n**How dare you threaten to ban my master nigger!**\n\n__Your account has been hacked! Pay 6969$ to my master__ [Indomie](tg://user?id=1036951071) __to release your account__😏")
         else:
             jnl = ("`Warning!!`"
                    "[{}](tg://user?id={})"
-                   f"` 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n\n"
+                   f"` 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {owner}\n\n"
                    "**Name: ** __{}__\n"
                    "**ID : ** `{}`\n"
                    ).format(firstname, idd, firstname, idd)
@@ -55,11 +50,11 @@ async def gbun(event):
             await reply_message.reply(jnl)
     else:
         mention = (
-            f"Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By {DEFAULTUSER} \nReason: No Reason Given. ")
+            f"Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By {owner} \nReason: No Reason Given. ")
         await event.reply(mention)
     await event.delete()
 
 CMD_HELP.update({
-    "fakegban": "`.fgban`\
-    \nUsage: Type .fgban or Reply .fgban reason and see it yourself. "
+    "fakegban": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}fgban`.\
+    \n↳ : Type .fgban or Reply .fgban reason and see it yourself. "
 })

1007ac4a46655544ab8a7a75c99cde249cffd2d5
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-07-08 04:16:21 +0700

    [+] IndomieUserbot

--- indomie/__main__.py
+++ indomie/__main__.py
@@ -20,16 +20,18 @@ from pytgcalls import __version__ as pytgcalls
 from pytgcalls import idle
 from telethon import version
 
-from userbot import BOT_TOKEN
-from userbot import BOT_VER as ubotversion
-from userbot import BOTLOG_CHATID, LOGS, bot
-from userbot.clients import memek_userbot_on, multimemek
-from userbot.core.git import git
-from userbot.modules import ALL_MODULES
-from userbot.modules.assistant import ASST_MODULES
-from userbot.utils import autobot, autopilot
+from indomie import BOT_TOKEN
+from indomie import BOT_VER as ubotversion
+from indomie import BOTLOG_CHATID, LOGS, bot
+from indomie.clients import memek_userbot_on, multimemek
+from indomie.core.git import git
+from indomie.modules import ALL_MODULES
+from indomie.modules.assistant import ASST_MODULES
+from indomie.utils import autobot, autopilot
+from indomie.utils import IndomieDB, HOSTED_ON, autobot, autopilot, indomie_version as ngewe
 
 try:
+    aduh = IndomieDB()
     client = multimemek()
     total = 5 - client
     git()
@@ -39,10 +41,10 @@ except BaseException as e:
     sys.exit(1)
 
 for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
+    imported_module = import_module("indomie.modules." + module_name)
 
 for module_name in ASST_MODULES:
-    imported_module = import_module("userbot.modules.assistant." + module_name)
+    imported_module = import_module("indomie.modules.assistant." + module_name)
 
 
 bot.loop.run_until_complete(memek_userbot_on())
@@ -56,7 +58,9 @@ if not BOT_TOKEN:
 LOGS.info(f"Python Version - {python_version()}")
 LOGS.info(f"Telethon Version - {version.__version__}")
 LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-LOGS.info(f"IndomieUserbot Version - {ubotversion} [ BERHASIL DIAKTIFKAN! ]")
+LOGS.info(f"Userbot Version - {ubotversion} •[{aduh.name}]•")
+LOGS.info(f"IndomieUserbot Version - {ngewe} •[{HOSTED_ON}]•")
+LOGS.info("[ BERHASIL DIAKTIFKAN! ]"
 idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()

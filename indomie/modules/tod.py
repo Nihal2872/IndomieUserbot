6038a4279cf67d0c4655d0da1f9b2e3c7fdc7cd3
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-07-08 05:00:07 +0700

    [+] fix tod

--- indomie/modules/tod.py
+++ indomie/modules/tod.py
@@ -26,20 +26,20 @@ async def truth_or_dare(tord):
     trod = tord.pattern_match.group(1).strip()
     troll = choice(Tod)
     if trod == "":
-        await tord.edit("    __Truth Or Dare ???__\n\n__Didapatkan Secara Acak__\n**      »» {troll} ««**")
+        await tord.edit(f"    __Truth Or Dare ???__\n\n__Didapatkan Secara Acak__\n**      »» {troll} ««**")
 
     if trod == "truth":
-        ah = await eor(tord, ("__Memproses Truth__")
+        ah = await eor(tord, "__Memproses Truth__")
         sleep(1)
         trth = random.choice(t)
-        await ah.edit("__Mendapatkan Hasil Truth__\n\n**»** __Truth__ :\n**»** __{trth}__")
+        await ah.edit(f"__Mendapatkan Hasil Truth__\n\n**»** __Truth__ :\n**»** __{trth}__")
         return
 
     if trod == "dare":
-        uh = await eor(tord, ("__Memproses Dare__")
+        uh = await eor(tord, "__Memproses Dare__")
         sleep(1)
         dr = random.choice(d)
-        await uh.edit("__Mendapatkan Hasil Dare Tod__\n\n**»** __Dare__ :\n**»** __{dr}__")
+        await uh.edit(f"__Mendapatkan Hasil Dare Tod__\n\n**»** __Dare__ :\n**»** __{dr}__")
 
         return
 

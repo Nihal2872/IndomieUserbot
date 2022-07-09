18b670ef2848b806f232f58de1b8b877a413876a
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-07-09 22:49:17 +0700

    [+] update help inline

--- indomie/modules/__help.py
+++ indomie/modules/__help.py
@@ -17,60 +17,70 @@
 # Thanks @Spechide.
 
 from telethon.errors.rpcerrorlist import BotInlineDisabledError as noinline
+from telethon.errors.rpcerrorlist import BotResponseTimeoutError as timout
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
 
 from indomie import CMD_HANDLER as cmd
-from indomie import bot, tgbot
+from indomie import bot, tgbot, ch2
 from indomie.utils import edit_or_reply, indomie_cmd
 
 
-@indomie_cmd(pattern="helps")
-async def _(event):
-    if event.fwd_from:
-        return
-    kontol = await tgbot.get_me()
-    BOT_USERNAME = kontol.username
-    if BOT_USERNAME is not None:
-        chat = "@Botfather"
-        try:
-            results = await event.client.inline_query(BOT_USERNAME, "@IndomieUserbot")
-            await results[0].click(
-                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-            )
-            await event.delete()
-        except noinline:
-            xx = await edit_or_reply(
-                event,
-                "**Mode Inline Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
-            )
-            async with bot.conversation(chat) as conv:
-                try:
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    await event.client(UnblockRequest(chat))
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                await xx.edit(
-                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helpme` **lagi untuk membuka menu bantuan.**"
-                )
-            await bot.delete_messages(
-                conv.chat_id,
-                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
-            )
+@indomie_cmd(pattern="helps(?: |$)(.*)")
+async def indomie(event):
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await edit_or_reply(event, f"{CMD_HELP[args]}\n\n© {ch2}")
+        else:
+            await edit_delete(event, f"`{args}`**NGETIK YANG BENER NGENTOT!!.**")
     else:
-        await edit_or_reply(
-            event,
-            "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
-        )
+        kontol = await tgbot.get_me()
+        BOT_USERNAME = kontol.username
+        if BOT_USERNAME is not None:
+            chat = "@Botfather"
+            try:
+                results = await event.client.inline_query(BOT_USERNAME, "@IndomieUserbot")
+                await results[0].click(
+                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                )
+                await event.delete()
+            except timout:
+                return await edit_or_reply(
+                    event,
+                    f"Bot tidak dapat menanggapi inline help.\nSilahkan Ketik `{cmd}restart`"
+            except noinline:
+                xx = await edit_or_reply(
+                    event,
+                    "**Mode Inline Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
+                )
+                async with bot.conversation(chat) as conv:
+                    try:
+                        first = await conv.send_message("/setinline")
+                        second = await conv.get_response()
+                        third = await conv.send_message(BOT_USERNAME)
+                        fourth = await conv.get_response()
+                        fifth = await conv.send_message("Search")
+                        sixth = await conv.get_response()
+                        await bot.send_read_acknowledge(conv.chat_id)
+                    except YouBlockedUserError:
+                        await event.client(UnblockRequest(chat))
+                        first = await conv.send_message("/setinline")
+                        second = await conv.get_response()
+                        third = await conv.send_message(BOT_USERNAME)
+                        fourth = await conv.get_response()
+                        fifth = await conv.send_message("Search")
+                        sixth = await conv.get_response()
+                        await bot.send_read_acknowledge(conv.chat_id)
+                    await xx.edit(
+                        f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helps` **lagi untuk membuka menu bantuan.**"
+                    )
+                await bot.delete_messages(
+                    conv.chat_id,
+                    [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
+                )
+        else:
+            await edit_or_reply(
+                event,
+                "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
+            )

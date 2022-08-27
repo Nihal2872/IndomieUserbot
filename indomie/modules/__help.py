1648b5238af0ea14d2036c25ff684f0fda93c80a
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-08-27 20:43:03 +0700

    Update __help.py

--- indomie/modules/__help.py
+++ indomie/modules/__help.py
@@ -16,61 +16,29 @@
 # @Qulec tarafından yazılmıştır.
 # Thanks @Spechide.
 
-from telethon.errors.rpcerrorlist import BotInlineDisabledError as noinline
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.contacts import UnblockRequest
 
-from indomie import CMD_HANDLER as cmd
-from indomie import bot, tgbot
-from indomie.utils import edit_or_reply, indomie_cmd
+from Indomie import BOT_USERNAME, CMD_HELP, bot, ch2
+from Indomie.utils import edit_or_reply, edit_delete, indomie_cmd
 
 
-@indomie_cmd(pattern="helpme")
-async def _(event):
-    if event.fwd_from:
-        return
-    pepek = await tgbot.get_me()
-    BOT_USERNAME = pepek.username
-    if BOT_USERNAME is not None:
-        chat = "@Botfather"
+@indomie_cmd(pattern="helpme ?(.*)")
+async def cmd_list(event):
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await edit_or_reply(event, f"**✦ Commands available in {args} ✦** \n\n" + str(CMD_HELP[args]) + "\n\n© {ch2}")
+        else:
+            await edit_delete(event, f"Plugins `{args}` tidak di temukan. **NGETIK YANG BENER NGENTOT!!.**")
+    else:
         try:
-            results = await event.client.inline_query(BOT_USERNAME, "@IndomieProject")
+            results = await bot.inline_query(  # pylint:disable=E0602
+                BOT_USERNAME, "@IndomieUserbot"
+            )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-        except noinline:
-            xx = await edit_or_reply(
-                event,
-                "**Inline Mode Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
-            )
-            async with bot.conversation(chat) as conv:
-                try:
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                except YouBlockedUserError:
-                    await event.client(UnblockRequest(chat))
-                    first = await conv.send_message("/setinline")
-                    second = await conv.get_response()
-                    third = await conv.send_message(BOT_USERNAME)
-                    fourth = await conv.get_response()
-                    fifth = await conv.send_message("Search")
-                    sixth = await conv.get_response()
-                    await bot.send_read_acknowledge(conv.chat_id)
-                await xx.edit(
-                    f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}helpme` **lagi untuk membuka menu bantuan.**"
-                )
-            await bot.delete_messages(
-                conv.chat_id,
-                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
-            )
-    else:
-        await edit_or_reply(
-            event,
-            "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
-        )
+        except BaseException:
+            await edit_delete(event,
+                              f"**INLINE MODE KAMU BELUM AKTIF!!**\n** Silahkan Ikuti Tutorial dibawah ini Untuk Menyalakan Inline Mode Kamu.**\n\n**© Tutorial Untuk Menyalakan Inline Mode Kamu :**\n**❖ Silahkan pergi ke bot @BotFather ketikan** '/mybots'\n**❖ Kemudian pilih bot Assistant mu yang ada di group log**\n**❖ Lalu pilih Bot Settings > Pilih inline Mode > pilih Turn on**\n**❖ Setelah itu Pergi ke group log atau group ini lagi**\n**Ketik** `.helpme` **lagi untuk membuka menu bantuan modules nya.**"
+                              )
